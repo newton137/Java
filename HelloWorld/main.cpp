986442367b49c4736167ec833f29103bea581282
newton137 <162945371+newton137@users.noreply.github.com>
2024-03-17 16:34:30 -0600

    Delete HelloWorld directory

--- HelloWorld/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-    cout << "Hello world!" <<5+5<< endl;
-    return 0;
-}
--- HelloWorld/src/com/f3nix/helloworld/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.f3nix.helloworld;
-
-public class HelloWorld {
-public static void main(String []args){
-	int variable = 10;
-	System.out.print("Hello World "+variable);
-}
-}
